125348c09642ca508ade0be732abdbd3d3957d89
Eric <emetcalf.pfx@gmail.com>
2013-11-08 11:25:01 -0600

    Add javadoc, fadeIn, and dynamically bind tree for any number of branches.

--- StoryTaskerUI/main.js
+++ StoryTaskerUI/main.js
@@ -1,20 +1,38 @@
-bindListItems("ul.entities > li");
-bindListItems("ul.stories > li");
+bindList($("ul.entities"));
 $(".title").bind("click", {
 	listItems : $("ul.entities > li")
 }, function(event) {
 	resetListItems(event.data.listItems)
 });
 
-function bindListItems(listItems) {
-	$(listItems).each(function() {
-		$(this).children("a").bind("click", {
-			listItems : listItems,
-			selectedItem : this
-		}, toggleActive);
-	});
+/**
+ * Binds listItems to hide/show on click.
+ * 
+ * @param list
+ *            jQuery object of list element.
+ */
+function bindList(list) {
+	if (list == undefined) {
+		return;
+	} else {
+		var listItems = list.children("li");
+		listItems.each(function() {
+			$(this).children("a").bind("click", {
+				listItems : listItems,
+				selectedItem : this
+			}, toggleActive);
+			bindList($(this).children("ul"));
+		});
+	}
 }
 
+/**
+ * Toggles list item between active and not along with hiding/showing it's
+ * sub-list elements.
+ * 
+ * @param event
+ *            Include selectedItem and listItems in data.
+ */
 function toggleActive(event) {
 	if ($(event.data.selectedItem).hasClass("active")) {
 		var childListItems = $(event.data.selectedItem).children("ul")
@@ -22,24 +40,40 @@ function toggleActive(event) {
 		resetListItems(childListItems);
 	} else {
 		makeItemActive(event.data.selectedItem);
-		hideAllItemsButI(event.data.listItems, event.data.selectedItem);
+		hideAllItemsButI(event.data.listItems, event.data.selectedItem.id);
 	}
 }
 
+/**
+ * Resets given list items to not be visible.
+ * 
+ * @param listItems
+ */
 function resetListItems(listItems) {
 	listItems.removeClass("active");
 	listItems.children("ul").fadeOut();
 	listItems.fadeIn();
 }
 
+/**
+ * Activates list item so visible.
+ * 
+ * @param listItem
+ */
 function makeItemActive(listItem) {
 	$(listItem).addClass("active");
 	$(listItem).children("ul").fadeIn();
 }
 
-function hideAllItemsButI(listItems, selectedItem) {
+/**
+ * Hides all items except item with given selected item id.
+ * 
+ * @param listItems
+ * @param selectedItemId
+ */
+function hideAllItemsButI(listItems, selectedItemId) {
 	$(listItems).each(function() {
-		if (this.id != selectedItem.id) {
+		if (this.id != selectedItemId) {
 			$(this).fadeOut();
 		}
 	});
